e774b88106c78f726b682847fcbe5f2f1d109974
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2021-07-05 11:39:33 +0200

    new script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const slide = document.getElementById('slide');
 const priceEl = document.getElementById('price-el')
 const views = document.getElementById('views');
@@ -36,3 +37,36 @@ slide.addEventListener('input', () => {
             break;
     }
 })
+*/
+const viewsElem = document.getElementById('views');
+const priceElem = document.getElementById('price-el');
+const toggleElem = document.getElementById('toggle');
+
+window.oninput = () => {
+    // Initialize results
+    let price, views;
+    // Get slider value
+    const sliderValue = Number(document.getElementById('slide').value);
+    // Checking selected value
+    if (sliderValue < 24) {
+        price = 8;
+        views = '10K';
+    } else if (sliderValue < 49) {
+        price = 12;
+        views = '50K';
+    } else if (sliderValue < 74) {
+        price = 16;
+        views = '100K';
+    } else if (sliderValue < 100) {
+        price = 24;
+        views = '500K';
+    } else {
+        price = 36;
+        views = '1M';
+    }
+    // Apply discount
+    price *= (toggleElem.checked)? .75 : 1
+    // Setting values
+    priceElem.innerHTML = `$${price}.00`
+    viewsElem.innerHTML = `${views}`
+}

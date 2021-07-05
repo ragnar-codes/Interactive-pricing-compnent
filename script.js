c61dc294a7975263146dd4613ba21e7715b89588
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2021-07-05 11:41:16 +0200

    final changes on script.js

--- script.js
+++ script.js
@@ -1,43 +1,4 @@
-/*
-const slide = document.getElementById('slide');
-const priceEl = document.getElementById('price-el')
-const views = document.getElementById('views');
-function initial(){
-    slide.value = 8;
-    priceEl.textContent = slide.value + "$";
-    views.textContent = "10K pageviews"
-}
-slide.addEventListener('input', () => {
-    const slideVal = document.getElementById('slide').value;
-    priceEl.textContent = slideVal + '$';
-    switch(priceEl.textContent){
-        case '8$':
-            views.textContent = '10K pageviews';
-            break;
-        case '12$':
-            views.textContent = '50K pageviews';
-            break;
-        case '16$':
-            views.textContent = '100K pageviews';
-            break;
-        case '20$':
-            views.textContent = '400K pageviews';
-            break;
-        case '24$':
-            views.textContent = '500K pageviews';
-            break;
-        case '28$':
-            views.textContent = '700K pageviews';
-            break;
-        case '32$':
-            views.textContent = '900K pageviews';
-            break;
-        case '36$':
-            views.textContent = '1M pageviews';
-            break;
-    }
-})
-*/
+
 const viewsElem = document.getElementById('views');
 const priceElem = document.getElementById('price-el');
 const toggleElem = document.getElementById('toggle');
